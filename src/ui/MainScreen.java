63b26b698e8b3bae432b521747050fefd169f978
Jywoac <jyriwiik@gmail.com>
2016-05-25 13:03:35 +0300

    Added customized buttons

--- src/ui/MainScreen.java
+++ src/ui/MainScreen.java
@@ -31,6 +31,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import java.awt.Font;
 
 public class MainScreen extends JFrame {
 
@@ -81,6 +82,7 @@ public class MainScreen extends JFrame {
 		mainMenu.add(verticalStrut);
 		
 		JLabel GameNameLabel = new JLabel("Labyrinth");
+		GameNameLabel.setFont(new Font("Sitka Small", Font.PLAIN, 18));
 		GameNameLabel.setForeground(Color.WHITE);
 		mainMenu.add(GameNameLabel);
 		GameNameLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -90,7 +92,14 @@ public class MainScreen extends JFrame {
 		mainMenu.add(buttonPanel);
 		buttonPanel.setLayout(null);
 		
-		JButton startGameButton = new JButton("Start");
+		JButton startGameButton = new JButton("");
+		startGameButton.setOpaque(false);
+		startGameButton.setContentAreaFilled(false);
+		startGameButton.setBorderPainted(false);
+		startGameButton.setFocusPainted(false);		
+		startGameButton.setIcon(new ImageIcon(MainScreen.class.getResource("/start.png")));
+		startGameButton.setPressedIcon(new ImageIcon(MainScreen.class.getResource("/start_pressed.png")));
+		startGameButton.setRolloverIcon(new ImageIcon(MainScreen.class.getResource("/start_rollover.png")));
 		startGameButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				//BoardActions bo = new BoardActions();
@@ -103,7 +112,14 @@ public class MainScreen extends JFrame {
 		startGameButton.setSize(new Dimension(100, 30));
 		buttonPanel.add(startGameButton);
 		
-		JButton controlsMenuButton = new JButton("Controls");
+		JButton controlsMenuButton = new JButton("");
+		controlsMenuButton.setOpaque(false);
+		controlsMenuButton.setContentAreaFilled(false);
+		controlsMenuButton.setBorderPainted(false);
+		controlsMenuButton.setFocusPainted(false);
+		controlsMenuButton.setPressedIcon(new ImageIcon(MainScreen.class.getResource("/controls_pressed.png")));
+		controlsMenuButton.setRolloverIcon(new ImageIcon(MainScreen.class.getResource("/controls_rollover.png")));
+		controlsMenuButton.setIcon(new ImageIcon(MainScreen.class.getResource("/controls.png")));
 		controlsMenuButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// move to controls screen with a new card
@@ -116,7 +132,14 @@ public class MainScreen extends JFrame {
 		controlsMenuButton.setSize(new Dimension(100, 30));
 		buttonPanel.add(controlsMenuButton);
 		
-		JButton exitGameButton = new JButton("Exit");
+		JButton exitGameButton = new JButton("");
+		exitGameButton.setOpaque(false);
+		exitGameButton.setContentAreaFilled(false);
+		exitGameButton.setBorderPainted(false);
+		exitGameButton.setFocusPainted(false);
+		exitGameButton.setIcon(new ImageIcon(MainScreen.class.getResource("/exit.png")));
+		exitGameButton.setPressedIcon(new ImageIcon(MainScreen.class.getResource("/exit_pressed.png")));
+		exitGameButton.setRolloverIcon(new ImageIcon(MainScreen.class.getResource("/exit_rollover.png")));
 		exitGameButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
@@ -146,11 +169,13 @@ public class MainScreen extends JFrame {
 		controlTextPanel.setLayout(new GridLayout(0, 1, 0, 0));
 		
 		JLabel lblUseTheArrowkeys = new JLabel("Use the arrowkeys to move.");
+		lblUseTheArrowkeys.setFont(new Font("Sitka Small", Font.PLAIN, 18));
 		controlTextPanel.add(lblUseTheArrowkeys);
 		lblUseTheArrowkeys.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUseTheArrowkeys.setForeground(Color.WHITE);
 		
 		JLabel lblNewLabel = new JLabel("Press the L key to look around.");
+		lblNewLabel.setFont(new Font("Sitka Small", Font.PLAIN, 18));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setForeground(Color.WHITE);
 		controlTextPanel.add(lblNewLabel);
@@ -160,7 +185,14 @@ public class MainScreen extends JFrame {
 		controlsMenuButtons.setBackground(Color.BLACK);
 		controls.add(controlsMenuButtons);
 		
-		JButton btnBack = new JButton("Back");
+		JButton btnBack = new JButton("");
+		btnBack.setOpaque(false);
+		btnBack.setContentAreaFilled(false);
+		btnBack.setBorderPainted(false);
+		btnBack.setFocusPainted(false);
+		btnBack.setIcon(new ImageIcon(MainScreen.class.getResource("/back.png")));
+		btnBack.setPressedIcon(new ImageIcon(MainScreen.class.getResource("/back_pressed.png")));
+		btnBack.setRolloverIcon(new ImageIcon(MainScreen.class.getResource("/back_rollover.png")));
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			    CardLayout cl = (CardLayout)(differentScreens.getLayout());
@@ -191,6 +223,7 @@ public class MainScreen extends JFrame {
 		textPanel.setLayout(new GridLayout(0, 1, 0, 0));
 		
 		JLabel lblChooseTheSize = new JLabel("Choose the size of the maze");
+		lblChooseTheSize.setFont(new Font("Sitka Small", Font.PLAIN, 18));
 		lblChooseTheSize.setHorizontalAlignment(SwingConstants.CENTER);
 		lblChooseTheSize.setForeground(Color.WHITE);
 		textPanel.add(lblChooseTheSize);
@@ -200,7 +233,14 @@ public class MainScreen extends JFrame {
 		mazeSizeButtons.setBackground(Color.BLACK);
 		gameSizeChoosingMenu.add(mazeSizeButtons);
 		
-		JButton backToMainMenu = new JButton("Back");
+		JButton backToMainMenu = new JButton("");
+		backToMainMenu.setOpaque(false);
+		backToMainMenu.setContentAreaFilled(false);
+		backToMainMenu.setBorderPainted(false);
+		backToMainMenu.setFocusPainted(false);
+		backToMainMenu.setIcon(new ImageIcon(MainScreen.class.getResource("/back.png")));
+		backToMainMenu.setPressedIcon(new ImageIcon(MainScreen.class.getResource("/back_pressed.png")));
+		backToMainMenu.setRolloverIcon(new ImageIcon(MainScreen.class.getResource("/back_rollover.png")));
 		backToMainMenu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			    CardLayout cl = (CardLayout)(differentScreens.getLayout());
@@ -208,7 +248,16 @@ public class MainScreen extends JFrame {
 			}
 		});
 		
-		JButton smallMaze = new JButton("Small");
+		JButton smallMaze = new JButton("");
+		smallMaze.setPressedIcon(new ImageIcon(MainScreen.class.getResource("/small_pressed.png")));
+		smallMaze.setRolloverIcon(new ImageIcon(MainScreen.class.getResource("/small_rollover.png")));
+		smallMaze.setForeground(Color.BLACK);
+		smallMaze.setBackground(Color.BLACK);
+		smallMaze.setIcon(new ImageIcon(MainScreen.class.getResource("/small.png")));
+		smallMaze.setOpaque(false);
+		smallMaze.setContentAreaFilled(false);
+		smallMaze.setBorderPainted(false);
+		smallMaze.setFocusPainted(false);
 		smallMaze.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
@@ -217,7 +266,14 @@ public class MainScreen extends JFrame {
 		smallMaze.setBounds(236, 41, 100, 30);
 		mazeSizeButtons.add(smallMaze);
 		
-		JButton mediumMaze = new JButton("Medium");
+		JButton mediumMaze = new JButton("");
+		mediumMaze.setIcon(new ImageIcon(MainScreen.class.getResource("/medium.png")));
+		mediumMaze.setPressedIcon(new ImageIcon(MainScreen.class.getResource("/medium_pressed.png")));
+		mediumMaze.setRolloverIcon(new ImageIcon(MainScreen.class.getResource("/medium_rollover.png")));
+		mediumMaze.setOpaque(false);
+		mediumMaze.setContentAreaFilled(false);
+		mediumMaze.setBorderPainted(false);
+		mediumMaze.setFocusPainted(false);
 		mediumMaze.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -226,13 +282,20 @@ public class MainScreen extends JFrame {
 		mediumMaze.setBounds(346, 41, 100, 30);
 		mazeSizeButtons.add(mediumMaze);
 		
-		JButton bigMaze = new JButton("Big");
+		JButton bigMaze = new JButton("");
+		bigMaze.setIcon(new ImageIcon(MainScreen.class.getResource("/large.png")));
+		bigMaze.setPressedIcon(new ImageIcon(MainScreen.class.getResource("/large_pressed.png")));
+		bigMaze.setRolloverIcon(new ImageIcon(MainScreen.class.getResource("/large_rollover.png")));
 		bigMaze.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		bigMaze.setSize(new Dimension(100, 30));
 		bigMaze.setBounds(456, 41, 100, 30);
+		bigMaze.setOpaque(false);
+		bigMaze.setContentAreaFilled(false);
+		bigMaze.setBorderPainted(false);
+		bigMaze.setFocusPainted(false);
 		mazeSizeButtons.add(bigMaze);
 		backToMainMenu.setSize(new Dimension(100, 30));
 		backToMainMenu.setBounds(346, 82, 100, 30);
